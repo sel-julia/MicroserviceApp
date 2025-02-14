f95bd7fea8a086a7155e7ef04f0748b15c42ea40
Yuliya_Seliukova <yuliya_seliukova@epam.com>
2025-02-14 18:09:29 +0300

    Introduction to Microservices #1: Hometask - code review changes - part 2

--- resource-service/src/main/java/org/resource/exception/ErrorResponse.java
+++ resource-service/src/main/java/org/resource/exception/ErrorResponse.java
@@ -1,5 +1,6 @@
 package org.resource.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Data;
 
@@ -7,6 +8,7 @@ import java.util.Map;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErrorResponse {
     private String errorMessage;
     private int errorCode;
--- resource-service/src/main/java/org/resource/exception/GlobalExceptionHandler.java
+++ resource-service/src/main/java/org/resource/exception/GlobalExceptionHandler.java
@@ -10,12 +10,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler({ConstraintViolationException.class, MethodArgumentTypeMismatchException.class,
-            NumberFormatException.class, HttpMediaTypeNotSupportedException.class})
-    public ResponseEntity<ErrorResponse> handleConstraintViolationException(Exception ex) {
+            NumberFormatException.class})
+    public ResponseEntity<ErrorResponse> handleValidationException(Exception ex) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+
+        var errorResponseDTO = ErrorResponse.builder()
+                .errorMessage("Invalid resource ID: must be a positive whole number")
+                .errorCode(status.value())
+                .build();
+
+        return ResponseEntity.status(status).body(errorResponseDTO);
+    }
+
+    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException ex) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
 
         var errorResponseDTO = ErrorResponse.builder()
@@ -25,6 +40,7 @@ public class GlobalExceptionHandler {
 
         return ResponseEntity.status(status).body(errorResponseDTO);
     }
+
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex) {
         HttpStatus status = HttpStatus.NOT_FOUND;
